6caa37cc6d3380db4c7a5bc31cc88f515a564f12
rus317521 <rus317521@yandex.ru>
2023-03-01 15:49:28 +0500

    modified Basket.java

--- src/Basket.java
+++ src/Basket.java
@@ -1,8 +1,8 @@
 import java.io.*;
 
 public class Basket implements Serializable {
-    protected static int[] prices; //Цены
-    protected static String[] productsInBasket; //Названия продуктов
+    protected int[] prices; //Цены
+    protected String[] productsInBasket; //Названия продуктов
     protected int[] amountProduct; //Количество продуктов
 
     //конструктор, принимающий массив цен и названий продуктов;
@@ -47,6 +47,9 @@ public class Basket implements Serializable {
         try (FileWriter writer = new FileWriter(textFile)
              //  FileReader inputStream = new FileReader(textFile)
         ) {
+            //Размер корзины
+            writer.write(prices.length);
+            writer.write("\n");
             //Продукты
             for (int j = 0; j < productsInBasket.length; j++) {
                 if (amountProduct[j] > 0) {
@@ -92,16 +95,12 @@ public class Basket implements Serializable {
     // метод восстановления объекта корзины из текстового файла,
     // в который ранее была она сохранена;
     public static Basket loadFromTxtFile(File textFile) {
-        int ap = prices.length;
-        String[] pricesBstr = new String[ap]; //Цены String
-        int[] pricesB = new int[ap]; //Цены int
-        String[] amountBstr = new String[ap]; //Количество  String
-        int[] amountB = new int[ap]; //Количество  int
-        String[] pricesBsumStr = new String[ap]; //суммарные Цены String
-        int[] pricesBsum = new int[ap]; //Суммарные Цены int
-        String[] productsInBasketB = new String[ap]; //Названия продуктов
-
-        Basket basket = new Basket(prices, productsInBasket);
+        int sizeBasket = 0;
+        String sizeBasketStr;
+        String[] productsInBasketB = null;
+        int[] amountB = null;
+        Basket basket = null;
+        int[] pricesB = null;
 
         try (BufferedReader br = new BufferedReader(new FileReader(textFile))) {
             //чтение построчно
@@ -109,31 +108,50 @@ public class Basket implements Serializable {
             String priseStr;
             String amountStr;
             String sumPriseStr;
+            //Считываем размер корзины
+            if ((sizeBasketStr = br.readLine()) != null) {
+                sizeBasket = Integer.parseInt(sizeBasketStr);
+                //Восстановить массив продуктов
+            }
 
+            int ap = sizeBasket;
+            String[] pricesBstr = new String[ap]; //Цены String
+            pricesB = new int[ap]; //Цены int
+            String[] amountBstr = new String[ap]; //Количество  String
+            amountB = new int[ap]; //Количество  int
+            String[] pricesBsumStr = new String[ap]; //суммарные Цены String
+            int[] pricesBsum = new int[ap]; //Суммарные Цены int
+            productsInBasketB = new String[ap]; //Названия продуктов
+
+
+            //Читаем строку с продуктами
             if ((productStr = br.readLine()) != null) {   //productStr = br.readLine();
                 productsInBasketB = productStr.split(" ");
                 //Восстановить массив продуктов
             }
+            //Цены на продукты
             if ((priseStr = br.readLine()) != null) {
-                if ((amountStr = br.readLine()) != null) {  // amountStr = br.readLine();
-                    amountBstr = amountStr.split(" ");
-                    for (int l = 0; l < amountBstr.length; l++) {
-                        amountB[l] = Integer.parseInt(amountBstr[l]);
-                    }
-
+                pricesBstr = priseStr.split(" ");
+                for (int l = 0; l < pricesBstr.length; l++) {
+                    pricesB[l] = Integer.parseInt(pricesBstr[l]);
                 }
+
+
             }
+            //Количество продуктов
+            if ((amountStr = br.readLine()) != null) {  // amountStr = br.readLine();
+                amountBstr = amountStr.split(" ");
+                for (int l = 0; l < amountBstr.length; l++) {
+                    amountB[l] = Integer.parseInt(amountBstr[l]);
+                }
+
+            }
+
+            basket = new Basket(pricesB, productsInBasketB, amountB);
+            //  return basket;
 
         } catch (IOException ex) {
             System.out.println(ex.getMessage());
-        }
-        for (int i = 0; i < productsInBasket.length; i++) {
-            for (int j = 0; j < productsInBasketB.length; j++) {
-                if (productsInBasket[i].equals(productsInBasketB[j])) {
-                    basket.addToCart(i + 1, amountB[j]);
-                }
-
-            }
         }
 
         return basket;
